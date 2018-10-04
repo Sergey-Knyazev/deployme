5611c7f04fe8afb0ec7068d2d4ad4bc0318c0fd3
root <root@alan.cs.gsu.edu>
2018-10-04 11:26:30 -0400

    wip

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -30,7 +30,7 @@ $("#submit-btn").on("click", function(){
     let lastName = $("#last-name").val().trim();
 
     $.ajax({
-        url: "/test.py",
+        url: "/deployme/test.py",
         type: "post",
         datatype: "json",
         async: false,
