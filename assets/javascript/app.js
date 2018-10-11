f1875ffa66b48dfed54a0a7cadc9cb4f83eaccaa
MMPark <parkmee@gmail.com>
2018-10-11 10:46:48 -0400

    updated logic and formatting

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -25,11 +25,16 @@ connection.connect((err) => {
   console.log('Connected!');
 }); */
 
+// PROFILE ENTRY PAGE ********************************
+
 $("#submit-btn").on("click", function(){
+    event.preventDefault();
     let firstName = $("#first-name").val().trim();
     let lastName = $("#last-name").val().trim();
 
-    $.ajax({
+    // DISABLED - ajax call to Tomcat server - need to enable server-side CORS connection
+
+    /* $.ajax({
         url: "/deployme/test.py",
         type: "post",
         datatype: "json",
@@ -42,8 +47,11 @@ $("#submit-btn").on("click", function(){
             alert(response.message);
             alert(response.keys);
         }
-    });
+    }); */
+    window.location.href = "thankyou.html"
 });
+
+// PROFILE ENTRY PAGE AND QUERY PAGE ********************
 
 $("#add-language-btn").on("click", function(){
     $(".language-box").append(
@@ -144,3 +152,74 @@ $("#add-skill-btn").on("click", function(){
         `
     )
 });
+
+// THANK YOU PAGE ********************************
+
+$("#return-btn").on("click", () => {
+    window.location.href = "index.html";
+})
+
+// QUERY PAGE *************************************
+
+$("#employee-type-select").change(() => {
+    console.log("hi");
+    if ($("#employee-type-select").val() === "Other") {
+        $("#employee-type-other-input").prop("disabled", false);
+    }
+});
+
+$("#qry-search-btn").on("click", () => {
+    event.preventDefault();
+    window.location.href = "query-results.html";
+})
+
+// STAFFING QUERY RESULTS PAGE ********************
+$("#qry-select-btn").on("click", () => {
+    event.preventDefault();
+    window.location.href = "status.html";
+})
+
+// DEPLOYMENT STATUS PAGE *************************
+$(document).on('click', '.confirmed-checkbox', function (){
+    const id = ($(this).attr('data-target'));
+    if ($(this).prop("checked")) {
+        $(`${id}`).text("Confirmed");
+    } else {
+        $(`${id}`).text("Pending");
+    }
+});
+
+// RESPONSE PAGE **********************************
+let count = 3;
+$("#add-response-btn").on("click", () => {
+    event.preventDefault();
+    $(".table-content").append(`
+        <tr>
+            <td class="red-text">Enter Response Name</td>
+            <td class="red-text">Enter Location</td>
+            <td class="red-text">Enter Language</td>
+            <td class="red-text">Enter Start Date</td>
+            <td></td>
+            <td>
+                <button data-toggle="collapse" type="button" data-target="#response-${count}-view" class="clickable">View</button>
+            </td>
+            <td>
+                <button data-toggle="collapse" type="button" data-target="#response-${count}-edit" class="clickable">Edit</button>
+            </td>
+        </tr>
+        <tr>
+            <td class="td-toggle" colspan="7">
+                <div id="response-${count}-view" class="collapse toggle-content text-center">
+                    No deployments assigned for this response
+                </div>
+            </td>
+        </tr>
+        <tr>
+            <td class="td-toggle" colspan="7">
+                <div id="response-${count}-edit" class="collapse toggle-content">form to edit response
+                    details</div>
+            </td>
+        </tr>
+    `);
+    count++;
+})
